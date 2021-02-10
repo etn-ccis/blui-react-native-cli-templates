070e1383c0b543a3701e0e03d2b3470cfb8e2e4e
daileytj <daileytj89@gmail.com>
2021-02-10 13:47:26 -0500

    Scaffold react native cli repo and add blank typescript template

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-native-cli-templates
+
+These are project templates that are used by the PX Blue CLI when starting a new PX Blue project.
---
+++ templates/blank-typescript/template/App.tsx
@@ -0,0 +1,157 @@
+import React, { useCallback } from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StyleSheet,
+    Linking,
+    TextStyle,
+    ViewStyle,
+    ImageStyle,
+    View,
+    Animated,
+    Easing,
+} from 'react-native';
+import { Button, Divider, Provider as ThemeProvider, useTheme } from 'react-native-paper';
+import * as PXBThemes from '@pxblue/react-native-themes';
+import { Body1, wrapIcon, Header, H4 } from '@pxblue/react-native-components';
+import { Theme } from 'react-native-paper/lib/typescript/types';
+import MatIcon from 'react-native-vector-icons/MaterialIcons';
+// import Logo from './assets/images/Logo.svg';
+
+const MenuIcon = wrapIcon({ IconClass: MatIcon, name: 'menu', flip: false });
+
+const styles = (
+    theme: Theme
+): StyleSheet.NamedStyles<{
+    content: ViewStyle;
+    pxbLogoWrapper: ViewStyle;
+    pxbLogo: Animated.WithAnimatedValue<ImageStyle>; // @TODO: Verify this type
+    title: TextStyle;
+    subtitle: TextStyle;
+    bold: TextStyle;
+    divider: ViewStyle;
+    openURLButtonText: TextStyle;
+}> =>
+    StyleSheet.create({
+        content: {
+            flex: 1,
+        },
+        pxbLogoWrapper: {
+            justifyContent: 'center',
+            marginTop: 16,
+        },
+        pxbLogo: {
+            alignSelf: 'center',
+        },
+        title: {
+            textAlign: 'center',
+            marginBottom: 16,
+        },
+        subtitle: {
+            textAlign: 'center',
+        },
+        bold: {
+            fontWeight: 'bold',
+        },
+        divider: {
+            marginVertical: 24,
+        },
+        openURLButtonText: {
+            color: theme.colors.text,
+            padding: 8,
+        },
+    });
+
+const OpenURLButton = (props: any): JSX.Element => {
+    const { url, title } = props;
+    const theme = useTheme();
+    const defaultStyles = styles(theme);
+
+    const handlePress = useCallback(async () => {
+        await Linking.openURL(url);
+    }, [url]);
+
+    return (
+        <Button
+            onPress={(): Promise<void> => handlePress()}
+            labelStyle={defaultStyles.openURLButtonText}
+            uppercase={false}
+        >
+            {title}
+        </Button>
+    );
+};
+
+const App = (): JSX.Element => {
+    const theme = useTheme();
+    const defaultStyles = styles(theme);
+    const spinValue = new Animated.Value(0);
+
+    Animated.loop(
+        Animated.timing(spinValue, {
+            toValue: 1,
+            duration: 2500,
+            easing: Easing.linear,
+            useNativeDriver: true,
+        })
+    ).start();
+
+    const spin = spinValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0deg', '360deg'],
+    });
+
+    return (
+        <ThemeProvider theme={PXBThemes.blue}>
+            <Header
+                title={'PX Blue React Native'}
+                styles={{ title: { marginBottom: -4 } }}
+                navigation={{
+                    icon: MenuIcon,
+                    onPress: (): void => {
+                        // do nothing
+                    },
+                }}
+            />
+            <SafeAreaView style={defaultStyles.content}>
+                <ScrollView>
+                    <View style={defaultStyles.pxbLogoWrapper}>
+                        <Animated.Image
+                            style={[defaultStyles.pxbLogo, { transform: [{ rotate: spin }] }]}
+                            source={require('./assets/images/pxblue-logo.png')}
+                        />
+                    </View>
+                    <H4 style={defaultStyles.title}>
+                        Welcome to PX <H4 color={'primary'}>Blue</H4>.
+                    </H4>
+                    <Body1 style={defaultStyles.subtitle}>
+                        Edit <Body1 style={defaultStyles.bold}>src/App.jsx</Body1> and save to reload.
+                    </Body1>
+                    <Divider style={defaultStyles.divider} />
+                    <OpenURLButton title={'PX Blue Documentation'} url={'https://pxblue.github.io/'} />
+                    <OpenURLButton
+                        title={'React Native Getting Started Guide'}
+                        url={'https://pxblue.github.io/development/frameworks-mobile/react-native'}
+                    />
+                    <OpenURLButton title={'Design Pattern Descriptions'} url={'https://pxblue.github.io/patterns'} />
+                    <OpenURLButton
+                        title={'PX Blue React Native Component Library'}
+                        url={'https://pxblue-components.github.io/react-native/'}
+                    />
+                    <OpenURLButton title={'Visit Us on GitHub'} url={'https://github.com/pxblue'} />
+                    <OpenURLButton
+                        title={'Design Pattern Source on GitHub'}
+                        url={'https://github.com/pxblue/react-native-design-patterns'}
+                    />
+                    <OpenURLButton title={'Release Roadmap'} url={'https://pxblue.github.io/roadmap'} />
+                    <OpenURLButton
+                        title={'Send Feedback or Suggestions'}
+                        url={'https://pxblue.github.io/community/contactus'}
+                    />
+                </ScrollView>
+            </SafeAreaView>
+        </ThemeProvider>
+    );
+};
+
+export default App;
