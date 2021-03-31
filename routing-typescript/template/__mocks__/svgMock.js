473ddf83fd58c8fb82c0935019e8bfe223259b17
joebochill <boyle.p.joseph@gmail.com>
2021-03-31 08:52:49 -0400

    Run prettier

--- routing-typescript/template/__mocks__/svgMock.js
+++ routing-typescript/template/__mocks__/svgMock.js
@@ -4,5 +4,5 @@
  * https://github.com/kristerkari/react-native-svg-transformer#usage-with-jest
  */
 
- module.exports = 'SvgMock'
- module.exports.ReactComponent = 'SvgMock'
+module.exports = 'SvgMock';
+module.exports.ReactComponent = 'SvgMock';
--- routing/template/jestSetupFile.js
+++ routing/template/jestSetupFile.js
@@ -2,13 +2,13 @@ import 'react-native-gesture-handler/jestSetup';
 
 jest.useFakeTimers();
 jest.mock('react-native-reanimated', () => {
-  const Reanimated = require('react-native-reanimated/mock');
+    const Reanimated = require('react-native-reanimated/mock');
 
-  // The mock for `call` immediately calls the callback which is incorrect
-  // So we override it with a no-op
-  Reanimated.default.call = () => {};
+    // The mock for `call` immediately calls the callback which is incorrect
+    // So we override it with a no-op
+    Reanimated.default.call = () => {};
 
-  return Reanimated;
+    return Reanimated;
 });
 
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
