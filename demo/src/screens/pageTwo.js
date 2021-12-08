08faa9bf76460f27abec838e009980e63c7b6570
Evan <evan.w.mclaughlin@gmail.com>
2021-12-08 14:19:37 -0500

    These are missing for some reason

---
+++ demo/src/screens/pageTwo.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
+import { EmptyState, Header } from '@pxblue/react-native-components';
+const Event = { name: 'event', direction: 'rtl' };
+const MenuIcon = { name: 'menu', direction: 'ltr' };
+
+const styles = () =>
+    StyleSheet.create({
+        content: {
+            flex: 1,
+        },
+        scrollViewContent: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+    });
+
+const PageTwo = ({ navigation }) => {
+    const defaultStyles = styles();
+
+    return (
+        <>
+            <Header
+                title={'Page Two'}
+                icon={MenuIcon}
+                onIconPress={() => {
+                    navigation.openDrawer();
+                }}
+            />
+            <SafeAreaView style={defaultStyles.content}>
+                <ScrollView contentContainerStyle={defaultStyles.scrollViewContent}>
+                    <EmptyState
+                        icon={Event}
+                        title={'Coming Soon'}
+                        description={'Replace this page with your own content'}
+                    />
+                </ScrollView>
+            </SafeAreaView>
+        </>
+    );
+};
+
+export default PageTwo;
