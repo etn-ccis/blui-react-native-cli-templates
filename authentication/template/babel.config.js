41ea74911759201886c071eafde174f547d280b1
Suraj Karambe <bkarambe@gmail.com>
2022-12-14 17:10:49 +0530

    Removed references for js templates

--- authentication/template/babel.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    presets: ['module:metro-react-native-babel-preset'],
-    // react-native-reanimated/plugin must be listed last
-    plugins: ['react-native-reanimated/plugin'],
-};
