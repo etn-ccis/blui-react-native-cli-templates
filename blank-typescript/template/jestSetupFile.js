c6a7bc793d7e83a2512c5b8eaa55b51f873f5ca4
Arshdeep Singh <arshdeepsingh@HMECMB00375.local>
2024-05-15 17:41:32 +0530

    testcase fix

---
+++ blank-typescript/template/jestSetupFile.js
@@ -0,0 +1,5 @@
+
+jest.useFakeTimers()
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
