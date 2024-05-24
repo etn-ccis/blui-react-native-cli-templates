16921222ad943036256bdcb62eb9980beee6cbc5
Arshdeep Singh <arshdeepsingh@HMECMB00375.local>
2024-05-24 16:57:32 +0530

    update workflow template

--- authentication-typescript/template/android/app/src/main/java/com/example/MainActivity.kt
+++ authentication-typescript/template/android/app/src/main/java/com/example/MainActivity.kt
@@ -1,23 +1,17 @@
-package com.workflow
+package com.example
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-import android.os.Bundle;
 
 class MainActivity : ReactActivity() {
-/** 
- * added for react-native-screens
-*/
-override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(null)
-  }
+
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "blankTemplate"
+  override fun getMainComponentName(): String = "example"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
